b3f2412926661fd33a3057a1a9743158c649a58a
AbrihamA <abriham.akele@gmail.com>
2025-02-07 19:33:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', (event) => {
                 
                 '/PTS_Flight_Logv1', // Ensure the start page is cached
                 '/PTS_Flight_Logv1/index.html',
+                '/PTS_Flight_Logv1/training_flights.html',
                 '/PTS_Flight_Logv1/styles.css',
                 '/PTS_Flight_Logv1/icons/icon-192x192.png',
                 '/PTS_Flight_Logv1/icons/icon-512x512.png'
