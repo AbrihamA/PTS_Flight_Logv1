4092092a1f9431d4a52fc4f750fbe5b1caf60c86
AbrihamA <abriham.akele@gmail.com>
2025-02-07 13:59:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('pwa-cache').then((cache) => {
             return cache.addAll([
-                '/',
+                
                 'index.html',
-                'styles.css',
-                'script.js'
+            
+                
             ]);
         })
     );
